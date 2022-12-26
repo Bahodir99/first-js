e1f864e5d5afc086accdb3cfa64075d2fc142d2b
Bahodir_99 <bahodirataboyev7@gmail.com>
2022-12-26 22:27:30 +0500

    first commit

---
+++ dars-1/js/main.js
@@ -0,0 +1,14 @@
+
+var name = prompt('ismingizni kiriting !');
+
+alert('Assalomu aleykum !' + name);
+
+var a = +prompt('1-son kiriting: ');
+
+var b = +prompt('2-son kiriting: ');
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
+console.log(a % b);
